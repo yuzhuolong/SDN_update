0c1cb2ded9e123448a21d5de79bbce65a5f4e862
yuzhuolong <yuzhuolong1993@gmail.com>
2015-11-01 18:05:24 +0800

    complete, pic todo...

--- Untitled.m
+++ Untitled.m
@@ -5,14 +5,27 @@
 %res
 %res.v2
 
-%%%%%%%%%%      environment setting   %%%%%%%%
+format long g;
 global nextNode pre numNode INFINITY
+DELTA = 1E-8;
+%%%%%%%%%%      environment setting   %%%%%%%%
+%%%%%%%%%%%     version 1 (small)     %%%%%%%%
+%{
 FLOWNUM = 5000;
-INFINITY = 100000;
+INFINITY = 10000000;
 QUANTITY = 5;
-TIMEMODIFY = 20;
-TIMEINSERT = 20;
-TIMELIMIT = 5000;
+TIMEMODIFY = 0.2;
+TIMEINSERT = 0.2;
+TIMELIMIT = 5;
+%}
+
+%%%%%%%%%%%     version 2 (big)     %%%%%%%%%%
+FLOWNUM = 200000;
+INFINITY = 10000000;
+QUANTITY = 3;
+TIMEMODIFY = 0.2;
+TIMEINSERT = 0.2;
+TIMELIMIT = 5;
 
 
 %%%%%%%%%%          get the graph   %%%%%%%%%%
@@ -36,7 +49,7 @@ roadY = zeros(1, numNode * numNode);
 for line = 1:1:numLine
     v = fscanf(fileIn, '%d', 1);
     u = fscanf(fileIn, '%d', 1);
-    if (cost(v,u) == INFINITY)
+    if (cost(v,u) >= INFINITY - DELTA)
         numRoad = numRoad + 1;
         roadX(numRoad) = u;
         roadY(numRoad) = v;
@@ -46,7 +59,7 @@ for line = 1:1:numLine
         nextNode(u, pre(u)) = v;
         pre(v) = pre(v) + 1;
         nextNode(v, pre(v)) = u;
-        capacity(u,v) = randi([20*1500,30*1500]);
+        capacity(u,v) = randi([2*15,3*15]);
         capacity(v,u) = capacity(u,v);
     end 
 end
@@ -61,7 +74,7 @@ for i = 1:1:FLOWNUM
     v = randArray(2);
     flowStart(i) = u;
     flowTerminal(i) = v;
-    flowSize(i) = randi([20,30]);
+    flowSize(i) = randi([20,30]) / 1000;
 end
 
 %%%%%%%%%%          create feasible pathes    %%%%%%%%%%%%
@@ -83,12 +96,12 @@ end
 inPath = zeros(numNode, numNode, QUANTITY, numRoad);
 for u = 1:1:numNode
     for v = 1:1:numNode
+        if u ~= v
         for p = 1:1:QUANTITY
-            tempPath = pathSet(u, v, p);
             i = 1;
             j = 2;
-            uu = tempPath(i);
-            vv = tempPath(j);
+            uu = pathSet(u,v,p,i);
+            vv = pathSet(u,v,p,j);
             while (vv ~= 0)
                 for tempRoad = 1:1:numRoad
                     uuu = roadX(tempRoad);
@@ -99,9 +112,10 @@ for u = 1:1:numNode
                 end
                 i = i + 1;
                 j = j + 1;
-                uu = tempPath(i);
-                vv = tempPath(j);
+                uu = pathSet(u,v,p,i);
+                vv = pathSet(u,v,p,j);
             end
+        end
         end
     end
 end
@@ -113,6 +127,23 @@ flowOSPF = zeros(1, FLOWNUM);
 for i = 1:1:FLOWNUM
     flowOSPF(i) = randi(QUANTITY);
 end
+lamdaOSPF = 0;
+for i = 1:1:numRoad
+    lamdaTemp = 0;
+    for j = 1:1:FLOWNUM
+        p = flowOSPF(j);
+        u = flowStart(j);
+        v = flowTerminal(j);
+        if inPath(u,v,p,i) - DELTA > 0
+            lamdaTemp = lamdaTemp + flowSize(j);
+        end
+    end
+    u = roadX(i);
+    v = roadY(i);
+    lamdaTemp = lamdaTemp / capacity(u,v);
+    lamdaOSPF = max(lamdaOSPF, lamdaTemp);
+end
+lamdaOSPF
 
 %%%%%%%%%%      obtain the update time on each switch (flow,path) %%%%%%%%
 timeCost = zeros(numNode, FLOWNUM,  QUANTITY);
@@ -124,6 +155,9 @@ for i = 1:1:FLOWNUM
         j = 2;
         tempNode = pathSet(u,v,p,j);
         while (tempNode ~= 0)
+            if tempNode <= numHost 
+                break;
+            end
             if (p ~= flowOSPF(i))
                 if (tempNode == w)
                     timeCost(tempNode, i, p) = TIMEMODIFY;
@@ -133,6 +167,9 @@ for i = 1:1:FLOWNUM
             end
             j = j + 1;
             tempNode = pathSet(u,v,p,j);
+            if tempNode <= numHost 
+                break;
+            end
         end
     end
 end
@@ -156,23 +193,25 @@ for i = 1:1:numRoad
         for p = 1:1:QUANTITY
             u = flowStart(j);
             v = flowTerminal(j);
-            if (inPath(u,v,p,i) > 0)
-                A(i, (j-1)*QUANTITY + p) = flowSize(j);
+            if (inPath(u,v,p,i) - DELTA > 0)
+                A(numNode + i, (j-1)*QUANTITY + p) = flowSize(j);
             end
         end
     end
-    A(i, -1) = -Capacity(roadX(i), roadY(i));
+    A(numNode + i, end) = -capacity(roadX(i), roadY(i));
 end
 
 b = zeros(numNode + numRoad, 1);
 for i = 1:1:numNode
     b(i, 1) = TIMELIMIT;
 end
+b
 
-Aeq = zeros(FLOWNUM, NN);
+%Aeq = zeros(FLOWNUM, NN);
+Aeq = sparse(FLOWNUM, NN);
 for i = 1:1:FLOWNUM
     for j = 1:1:QUANTITY
-        Aeq(i,(i-1) * QUANTITTY + j) = 1;
+        Aeq(i,(i-1) * QUANTITY + j) = 1;
     end
 end
 
@@ -180,16 +219,102 @@ beq = ones(FLOWNUM, 1);
 
 lb = zeros(NN, 1);
 ub = [ones(NN-1, 1); 10];
+tic;
+%options = optimset('Display' , 'off' , 'LargeScale' , 'off' , 'Simplex' , 'on');
+options = optimset('Display' , 'final' , 'LargeScale' , 'on' , 'Simplex' , 'on');
+resultGRSU = linprog(f,A,b,Aeq,beq,lb,ub,[],options);
+%resultGRSU = linprog(f,A,b,Aeq,beq,lb,ub);
+toc;
 
-options = optimset('Display' , 'off' , 'LargeScale' , 'off' , 'Simplex' , 'on');
-result = linprog(f,A,b,Aeq,beq,lb,ub,[],options);
+
+lamda = resultGRSU(end);
+lamda
+
+b0 = A * resultGRSU;
+b0
+beq0 = Aeq * resultGRSU;
+beq0
+%b0
+%A(:,1:10)
+
+A = zeros(numNode, NN);
+for i = 1:1:numNode
+    for j = 1:1:FLOWNUM
+        for p = 1:1:QUANTITY
+            A(i, (j-1)*QUANTITY + p) = timeCost(i, j, p);
+        end
+    end
+end
+timeDelay = A * resultGRSU;
+timeDelay
+delayCountGRSU = 0;
+for i = 1:1:numNode
+    if (timeDelay(i) > TIMELIMIT)
+        delay0 = timeDelay(i);
+       % delay0
+       % TIMELIMIT
+        delayCountGRSU = delayCountGRSU + 1;
+    end
+end
+delayCountGRSU
 
 %%%%%%%%%%%%%%      obtain the solution of MCF    %%%%%%%%%%%%%%%%
 
 
+f = zeros(NN, 1);
+f(end) = 1;
 
-    
+A = zeros(numRoad, NN);
 
+for i = 1:1:numRoad
+    for j = 1:1:FLOWNUM
+        for p = 1:1:QUANTITY
+            u = flowStart(j);
+            v = flowTerminal(j);
+            if (inPath(u,v,p,i) > 0)
+                A(i, (j-1)*QUANTITY + p) = flowSize(j);
+            end
+        end
+    end
+    A(i, end) = -capacity(roadX(i), roadY(i));
+end
+
+b = zeros(numRoad, 1);
+
+%Aeq = zeros(FLOWNUM, NN);
+Aeq = sparse(FLOWNUM, NN);
+for i = 1:1:FLOWNUM
+    for j = 1:1:QUANTITY
+        Aeq(i,(i-1) * QUANTITY + j) = 1;
+    end
+end
+
+beq = ones(FLOWNUM, 1);
+
+lb = zeros(NN, 1);
+ub = [ones(NN-1, 1); 10];
+tic;
+options = optimset('Display' , 'off' , 'LargeScale' , 'on' , 'Simplex' , 'on');
+resultMCF = linprog(f,A,b,Aeq,beq,lb,ub,[],options);
+toc;
+A = zeros(numNode, NN);
+for i = 1:1:numNode
+    for j = 1:1:FLOWNUM
+        for p = 1:1:QUANTITY
+            A(i, (j-1)*QUANTITY + p) = timeCost(i, j, p);
+        end
+    end
+end
+timeDelay = A * resultMCF(1:NN);
+delayCountMCF = 0;
+for i = 1:1:numNode
+    if (timeDelay(i) > TIMELIMIT)
+        delayCountMCF = delayCountMCF + 1;
+    end
+end
+delayCountMCF
+lamdaMCF = resultMCF(end);
+lamdaMCF
 
 
 
